b36afcd5c5aed52416596931659ce85a2a07d7d7
Youssefboulahia1999 <youssef.boulahia1999@gmail.com>
2024-10-15 11:47:52 +0200

    nouveau code

---
+++ code.py
@@ -0,0 +1,12 @@
+#not a git repository (or any of the parent directories): .git pas depot et de depot 
+# Fonction pour multiplier deux nombres
+def multiply(a, b):
+    return a * b
+
+# Exemple d'utilisation
+num1 = float(input("Entrez le premier nombre : "))
+num2 = float(input("Entrez le deuxième nombre : "))
+
+resultat = multiply(num1, num2)
+
+print(f"La multiplication de {num1} et {num2} donne : {resultat}")
